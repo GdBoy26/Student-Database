0d3cd50823d0e456328af31375d82a65ba43dda3
GdBoy26 <149719757+GdBoy26@users.noreply.github.com>
2025-04-27 22:10:59 +0530

    Update EditStudent.js

--- frontend/src/pages/EditStudent.js
+++ frontend/src/pages/EditStudent.js
@@ -18,7 +18,7 @@ const EditStudent = () => {
   useEffect(() => {
     const fetchStudent = async () => {
       try {
-        const res = await axios.get(`https://student-database-backend-d6z6.onrender.com/${id}`);
+        const res = await axios.get(`https://student-database-backend-ejr0.onrender.com/${id}`);
         setFormData(res.data);
       } catch (error) {
         toast.error("Error loading student data");
@@ -35,7 +35,7 @@ const EditStudent = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.put(`https://student-database-backend-d6z6.onrender.com/${id}`, formData);
+      await axios.put(`https://student-database-backend-ejr0.onrender.com/${id}`, formData);
       toast.success("Student Updated Successfully");
       navigate("/students");
     } catch (error) {
